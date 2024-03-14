2f79edb06b7fe7afd0f32fa2572108a6afdafa0a
unknown <sc.arandia@gmail.com>
2024-03-14 17:13:32 -0400

    Page back to normal

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,30 @@
 import React, { useState } from "react";
 import "./styles/styles.css";
 import expectedDB from "./expectedDB.json";
-import { AccountsTreeView } from "./components/AccountsTree";
 import { AccountForm } from "./components/AccountsForm";
-import { filterAccounts, filterForm, displayFilteredItems } from "./components/AccountsFilters";
-
+import { AccountsTreeView } from "./components/AccountsTree";
+import { filterAccounts } from "./components/AccountsFilters";
 
 export default function App() {
   // State to manage the filter type
   const [filterType, setFilterType] = useState("None");
-
-  // Filtered items based on the selected filter type
-  const filteredItems = expectedDB.accounts[0].items.filter(
-    (item) => {
-      console.log('Filtering item:', item);
-      console.log('Filter Type:', filterType);
-      return item.type === filterType;
-    }
-  );
 
   // Function to handle filter type change
   const handleFilterChange = (event) => {
     setFilterType(event.target.value);
   };
 
+  // Filtered items based on the selected filter type
+  const filteredItems = filterAccounts(expectedDB.accounts[0].items, filterType);
+
   return (
     <>
-      {/* only <accounts view> as a function*/}
-      {/* Account Form Component */}
-      <form className="new-item-form">
-        <div className="form-row">
-          <h3 className="header">New Transaction Information</h3>
-          <label htmlFor="type">Type</label>
-          <select name="type" id="type">
-            <option value="Haber">Haber</option>
-            <option value="Debe">Debe</option>
-            <option value="Patrimonio">Patrimonio</option>
-            <option value="Pasivos">Pasivos</option>
-          </select>
-        </div>
-        <div className="form-row">
-          <label htmlFor="item">Description</label>
-          <input type="text" id="item" />
-        </div>
-        <div className="form-row">
-          <label htmlFor="item">Sub-item of</label>
-          <input type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
-
-      {/* Accounts Tree Component */}
-      <h3 className="header">Accounts Tree </h3>
-      {/* Filter form */}
-      <form>
-        <div className="form-row">
-          <label htmlFor="filterType">Filter</label>
-          <select
-            name="type"
-            id="filterType"
-            value={filterType}
-            onChange={handleFilterChange}
-          >
-            <option value="None">None</option>
-            <option value="Haber">Haber</option>
-            <option value="Debe">Debe</option>
-            <option value="Patrimonio">Patrimonio</option>
-            <option value="Pasivos">Pasivos</option>
-          </select>
-        </div>
-      </form>
-
-      {/* Display filtered items */}
-      <div className="accountsTree">
-        <h3>Filtered Items (Type: {filterType})</h3>
-        <ul> {/* will be Account List*/}
-          {filteredItems.map((item, index) => (
-            <li key={index}> {/*  will be  Account List items*/}
-              <strong>Code: {item.code}</strong>
-              <p>Description: {item.description}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <AccountForm />
+      <AccountsTreeView
+        filterType={filterType}
+        filteredItems={filteredItems}
+        handleFilterChange={handleFilterChange}
+      />
     </>
   );
 }
--- src/components/AccountsTree.jsx
+++ src/components/AccountsTree.jsx
@@ -1,54 +1,8 @@
-import React, { useState } from "react";
-import "../styles/styles.css";
-import expectedDB from "../expectedDB.json";
-import { AccountsTreeView } from "./AccountsTree";
-import { AccountForm } from "./AccountsForm";
-import { filterAccounts, filterForm, displayFilteredItems } from "./AccountsFilters";
+import React from "react";
 
-
-export default function App() {
-  // State to manage the filter type
-  const [filterType, setFilterType] = useState("None");
-
-  // Filtered items based on the selected filter type
-  const filteredItems = expectedDB.accounts[0].items.filter(
-    (item) => {
-      console.log('Filtering item:', item);
-      console.log('Filter Type:', filterType);
-      return item.type === filterType;
-    }
-  );
-
-  // Function to handle filter type change
-  const handleFilterChange = (event) => {
-    setFilterType(event.target.value);
-  };
-
+export function AccountsTreeView({ filterType, filteredItems, handleFilterChange }) {
   return (
-    <>{/* only <accounts view> as a function*/}
-      {/* Account Form Component */}
-      <form className="new-item-form">
-        <div className="form-row">
-          <h3 className="header">New Transaction Information</h3>
-          <label htmlFor="type">Type</label>
-          <select name="type" id="type">
-            <option value="Haber">Haber</option>
-            <option value="Debe">Debe</option>
-            <option value="Patrimonio">Patrimonio</option>
-            <option value="Pasivos">Pasivos</option>
-          </select>
-        </div>
-        <div className="form-row">
-          <label htmlFor="item">Description</label>
-          <input type="text" id="item" />
-        </div>
-        <div className="form-row">
-          <label htmlFor="item">Sub-item of</label>
-          <input type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
-
+    <>
       {/* Accounts Tree Component */}
       <h3 className="header">Accounts Tree </h3>
       {/* Filter form */}
@@ -73,9 +27,9 @@ export default function App() {
       {/* Display filtered items */}
       <div className="accountsTree">
         <h3>Filtered Items (Type: {filterType})</h3>
-        <ul> {/* will be Account List*/}
+        <ul>
           {filteredItems.map((item, index) => (
-            <li key={index}> {/*  will be  Account List items*/}
+            <li key={index}>
               <strong>Code: {item.code}</strong>
               <p>Description: {item.description}</p>
             </li>
